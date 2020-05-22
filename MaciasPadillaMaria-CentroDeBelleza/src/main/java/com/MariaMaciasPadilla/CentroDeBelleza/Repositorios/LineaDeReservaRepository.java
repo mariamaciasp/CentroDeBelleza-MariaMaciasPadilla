a65b0a416b2dfc5d6e3e521bc2cc5f7d1396b9e1
Maria Macias <mariamaciaspadilla@gmail.com>
2020-05-23 01:16:14 +0200

    commit final proyecto, incluyendo archivos de entornos

--- MaciasPadillaMaria-CentroDeBelleza/src/main/java/com/MariaMaciasPadilla/CentroDeBelleza/Repositorios/LineaDeReservaRepository.java
+++
@@ -1,9 +0,0 @@
-package com.MariaMaciasPadilla.CentroDeBelleza.Repositorios;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.MariaMaciasPadilla.CentroDeBelleza.Modelo.LineaDeReserva;
-
-public interface LineaDeReservaRepository extends JpaRepository<LineaDeReserva, Long>{
-
-}
